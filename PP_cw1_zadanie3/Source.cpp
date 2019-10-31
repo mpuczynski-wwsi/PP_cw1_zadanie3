723fa9a99963a762cb06bead37fe7bda3d6a977a
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-10-31 20:02:14 +0100

    add parameters to progrma

--- PP_cw1_zadanie3/Source.cpp
+++ PP_cw1_zadanie3/Source.cpp
@@ -1,8 +1,32 @@
+﻿
+#define _USE_MATH_DEFINES
 #include<iostream>
 #include<cmath>
 #include<iomanip>
 using namespace std;
 
 int main() {
+	double wynik, stopnie, alfa, x, v0, y0;
+	const double G = 9.81;
 
+	cout << " wprowadz kat alfa w stopniach: ";
+	cin >> stopnie;
+
+	cout << "\n" <<" wprowadz przebyta  odleglosc x  w poziomie: ";
+	cin >> x;
+
+	cout << "\n" << " wprowadz predkosc poczatkowa, podawana w jednostce km/h: ";
+	cin >> v0;
+
+	v0 = v0 * 1000.0 / 3600;
+
+	cout << "\n" << " wprowadz wysokosc, z ktorej pilka zostaje rzucona, podawana w metrach: ";
+	cin >> y0;
+
+	alfa = stopnie * M_PI / 180.0;
+
+
+	wynik = x * tan(alfa) - (1.0 / 2 * v0 * v0) * (G * x * x / cos(alfa) * cos(alfa)) + y0;
+
+	cout << "\n" << "wysokosc, na ktorej znajduje sie pilka  po przebyciu odleglosci  w poziomie to " << wynik;
 }
